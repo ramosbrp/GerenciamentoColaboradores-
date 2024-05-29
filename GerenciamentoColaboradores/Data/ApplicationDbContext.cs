c5ff1fb8536d62087fd9c52a86c99f0f43da8f38
Bruno <bruno.rp45@gmail.com>
2024-05-29 16:02:58 -0300

    ApplicationDbContext

---
+++ GerenciamentoColaboradores/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GerenciamentoColaboradores.Models;
+
+namespace GerenciamentoColaboradores.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Colaborador> Colaboradores { get; set; }
+        public DbSet<Cargo> Cargos { get; set; }
+    }
+}
