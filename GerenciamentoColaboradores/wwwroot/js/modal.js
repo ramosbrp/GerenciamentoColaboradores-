a772f2fe549b922d448943cc1c89350c9d0de3a3
Bruno <bruno.rp45@gmail.com>
2024-05-29 17:27:55 -0300

    modal.js

---
+++ GerenciamentoColaboradores/wwwroot/js/modal.js
@@ -0,0 +1,18 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById('modal');
+    const modalForm = document.getElementById('modalForm');
+    const emailInput = document.getElementById('email');
+    const emailError = document.getElementById('emailError');
+    const createButton = document.getElementById('createButton');
+    const searchButton = document.getElementById('searchButton');
+    const cancelButton = document.getElementById('cancelButton');
+
+    function showModal(title, data = {}) {
+        document.getElementById('modalTitle').innerText = title;
+        modal.style.display = 'block';
+        document.getElementById('colaboradorId').value = data.id || '';
+        document.getElementById('nome').value = data.nome || '';
+        document.getElementById('email').value = data.email || '';
+        document.getElementBy
+    }
+});
